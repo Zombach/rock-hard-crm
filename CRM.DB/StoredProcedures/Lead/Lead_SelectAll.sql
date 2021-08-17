99a369cdf95480c805ff9ccb9a17e062288adfb6
Timur <Kotafrakt@gmail.com>
2021-08-17 15:04:00 +0300

    add procedures

---
+++ CRM.DB/StoredProcedures/Lead/Lead_SelectAll.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE dbo.Lead_SelectAll
+AS
+BEGIN
+	SELECT 
+		l.Id,
+		l.FirstName,
+		l.LastName,
+		l.Patronymic,
+		l.Email,
+		l.Role as Id
+	FROM dbo.[Lead] l
+	WHERE IsDeleted = 0
+END
