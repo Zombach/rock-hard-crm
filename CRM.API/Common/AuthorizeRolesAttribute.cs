ef5d7fbbc65392961218a8882cae36a4cafa876c
Eugene <Zhekul.90@gmail.com>
2021-08-20 11:47:53 +0300

    add AuthorizeRolesAttribute

---
+++ CRM.API/Common/AuthorizeRolesAttribute.cs
@@ -0,0 +1,14 @@
+﻿using CRM.DAL.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CRM.API.Common
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        public AuthorizeRolesAttribute(params Role[] roles) : base()
+        {
+            Roles = string.Join(",", roles);
+            Roles += $"{Role.Admin}";
+        }
+    }
+}
