ca6a7030766e12cd0b012517dcdcd6caf2980efa
Timur <Kotafrakt@gmail.com>
2021-08-26 00:26:46 +0300

    fix space

--- CRM.API/Extensions/ServiceCollectionExtensions.cs
+++ CRM.API/Extensions/ServiceCollectionExtensions.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 
-
 namespace CRM.API.Extensions
 {
     public static class ServiceCollectionExtensions
