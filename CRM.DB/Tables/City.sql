fefe1244f3fa1ed30b6b47c0d24e0708992b542f
Vadim <vki-13@yandex.ru>
2021-08-30 16:36:39 +0300

    fix cities table

--- CRM.DB/Tables/City.sql
+++ CRM.DB/Tables/City.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[City] (
     [Id]   INT           NOT NULL IDENTITY (1, 1),
-    [Name] NVARCHAR (50) NOT NULL,
+    [Name] NVARCHAR (50) NOT NULL UNIQUE,
     CONSTRAINT [PK_CITY] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
