d51d8708ae59730cb74ab113cb98f4a5829feabb
Timur <Kotafrakt@gmail.com>
2021-08-31 22:39:52 +0300

    fix input model

---
+++ CRM.API/Models/InputModels/TransferInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static CRM.API.Common.ValidationMessage;
+
+namespace CRM.API.Models
+{
+    public class TransferInputModel : TransactionInputModel
+    {
+        [Required(ErrorMessage = RecipientAccountRequired)]
+        public int RecipientId { get; set; }
+    }
+}
