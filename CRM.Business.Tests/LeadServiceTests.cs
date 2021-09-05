fd3b0048f43d1367d4dbb79c19fbfc5f0ecc8a3a
Nastya <kushkoeva99@gmail.com>
2021-09-05 19:47:39 +0300

    RHB-101: add setup for LeadServiceTests

---
+++ CRM.Business.Tests/LeadServiceTests.cs
@@ -0,0 +1,37 @@
+using CRM.Business.Options;
+using CRM.Business.Services;
+using CRM.DAL.Repositories;
+using DevEdu.Business.ValidationHelpers;
+using Moq;
+using NUnit.Framework;
+
+namespace CRM.Business.Tests
+{
+    public class Tests
+    {
+        private Mock<ILeadRepository> _leadRepoMock;
+        private Mock<IAccountRepository> _accountRepoMock;
+
+        private LeadService _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _leadRepoMock = new Mock<ILeadRepository>();
+            _accountRepoMock = new Mock<IAccountRepository>();
+            IAuthenticationService authenticationService = new AuthenticationService(_leadRepoMock.Object, new Mock<IAuthOptions>().Object);
+
+            _sut = new LeadService(
+                _leadRepoMock.Object, 
+                _accountRepoMock.Object, 
+                authenticationService, 
+                new LeadValidationHelper(_leadRepoMock.Object));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ CRM.Business.Tests/TestDataHelpers/LeadFilterData.cs
@@ -0,0 +1,6 @@
+﻿namespace CRM.Business.Tests.Data
+{
+    public static class LeadFilterData
+    {
+    }
+}
