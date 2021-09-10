c6d7519f0745ff0a5c2cab0c3c3d4dc3a35693be
ZLoo <Kozlov.s.v.1992@gmail.com>
2021-09-10 22:37:50 +0300

    Refact code

--- CRM.Business/Models/AccountBusinessModel.cs
+++ CRM.Business/Models/AccountBusinessModel.cs
@@ -1,11 +1,15 @@
 ﻿using CRM.DAL.Enums;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CRM.Business.Models
 {
     public class AccountBusinessModel
     {
+        private const string _recipientId = @"$.[?(@.RecipientAccountId)]";
+
         public int Id { get; set; }
         public int LeadId { get; set; }
         public Currency Currency { get; set; }
@@ -15,5 +19,16 @@ namespace CRM.Business.Models
         public List<TransactionBusinessModel> Transactions { get; set; }
         public List<TransferBusinessModel> Transfers { get; set; }
         public decimal Balance { get; set; }
+
+        
+
+        public static void GetListModels(string json)
+        {
+            var jArray = JArray.Parse(json);
+            AccountBusinessModelExtension.Transfers = jArray.Where(j => j.SelectToken(_recipientId) != null)
+                .Select(t => t.ToObject<TransferBusinessModel>()).ToList();
+            AccountBusinessModelExtension.Transactions = jArray.Where(j => j.SelectToken(_recipientId) == null)
+                .Select(t => t.ToObject<TransactionBusinessModel>()).ToList();
+        }
     }
 }
