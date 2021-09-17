fae31e305b5e97a19ebcdf9e0c815377a03b9521
Eugene <Zhekul.90@gmail.com>
2021-09-17 19:04:34 +0300

    add LeadIdentityInfoData

---
+++ CRM.Business.Tests/TestsDataHelpers/LeadIdentityInfoData.cs
@@ -0,0 +1,35 @@
+﻿using CRM.Business.IdentityInfo;
+using CRM.DAL.Enums;
+
+namespace CRM.Business.Tests.TestsDataHelpers
+{
+    public static class LeadIdentityInfoData
+    {
+        public static LeadIdentityInfo GetRegularLeadIdentityInfo()
+        {
+            return new ()
+            {
+                LeadId = 1,
+                Role = Role.Regular
+            };
+        }
+
+        public static LeadIdentityInfo GetVipLeadIdentityInfo()
+        {
+            return new ()
+            {
+                LeadId = 2,
+                Role = Role.Vip
+            };
+        }
+
+        public static LeadIdentityInfo GetAdminLeadIdentityInfo()
+        {
+            return new ()
+            {
+                LeadId = 3,
+                Role = Role.Admin
+            };
+        }
+    }
+}
