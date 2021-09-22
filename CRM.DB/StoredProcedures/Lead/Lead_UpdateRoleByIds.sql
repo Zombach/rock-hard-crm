924c2b7baabfaaa200cb5a92d5ebb6ff1e250c27
MariaMalyutina <merymal2696@gmail.com>
2021-09-23 00:44:00 +0300

    add sp in db project

---
+++ CRM.DB/StoredProcedures/Lead/Lead_UpdateRoleByIds.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[LeadsList_Role_Update]
+  @tblLeadDto LeadDtoType readonly 
+AS
+BEGIN
+  UPDATE dbo.[Lead]
+  SET 
+	Role = src.Role
+
+  FROM 
+	@tblLeadDto src
+  WHERE 
+	dbo.Lead.Id = src.LeadId
+End
